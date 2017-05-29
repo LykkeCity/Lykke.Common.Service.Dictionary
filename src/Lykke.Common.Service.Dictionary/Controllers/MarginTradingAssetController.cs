a21a6c54f3988034cf08b695bfc9eb606fcf25bf
Anton Sivov <anton.sivov@inbox.ru>
2017-05-29 21:53:28 +0300

    Init Commitment

---
+++ src/Lykke.Common.Service.Dictionary/Controllers/MarginTradingAssetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Common.Service.Dictionary.Controllers
+{
+    [Route("api/[controller]")]
+    public class MarginTradingAssetController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
